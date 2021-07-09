6ad558a6f10d44e902f4c51f7a399ca23645ea3d
Eden AI <67952486+ai-compare@users.noreply.github.com>
2021-07-09 15:09:55 +0200

    Update and rename AI-Compare-TextToSpeech-python-request.py to EdenAI-TextToSpeech-python-request.py

--- EdenAI-TextToSpeech-python-request.py
+++ EdenAI-TextToSpeech-python-request.py
@@ -1,17 +1,17 @@
 import requests
 
-# You can find the documentation here : https://www.ai-compare.com/v1/redoc/#operation/Text%20To%20Speech
+# You can find the documentation here : https://api.edenai.run/v1/redoc/#section/How-it-works
 
 #Enter your API Token
 headers = {'Authorization': 'Bearer your API Key'} #You can get your free API token here https://www.ai-compare.com/accounts/login/?next=/my_apis
 
 # Select API
-url = 'https://www.ai-compare.com/v1/pretrained/audio/text_to_speech'
+url = 'https://api.edenai.run/v1/pretrained/audio/text_to_speech'
 
 # Select providers, and audio to transcribe
-payload = {'providers':'[\'ibm\', \'cognitives_service\', \'aws\', \'google_cloud\']','text':'Hi, my name is Martin','language':'en-US', 'option':'MALE'}
+payload = {'providers':'[\'ibm\', \'microsoft\', \'aws\', \'google_cloud\']','text':'Hi, my name is Martin','language':'en-US', 'option':'MALE'}
 
-# Request to AI COMPARE
+# Request to Eden AI
 response = requests.request("POST", url, headers=headers, data = payload)
 
 # Print result
